0e5a4bd70562df9c02a827fa4ccd9bc3d78a287c
Fmancino <17271080+Fmancino@users.noreply.github.com>
2018-03-31 15:28:23 +0200

    better organization

---
+++ init/package-managers/apt-get.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#FILENAME: apt-get.sh
+
+#PURPOSE:
+
+#CREATION DATE: 31-03-2018
+
+#LAST MODIFIED: Sat 31 Mar 2018 03:27:03 PM CEST
+
+#AUTHOR: Francesco Mancino, francescoman@protonmail.com
+#-------------------------------------------------------------------------
+sudo apt-get install tree
