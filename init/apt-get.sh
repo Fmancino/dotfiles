bd7560155907042569db8573ce77b9ffcfcfc0b3
Fmancino <17271080+Fmancino@users.noreply.github.com>
2018-04-03 15:12:10 +0200

    plug.vim included in git repositor

--- init/apt-get.sh
+++ init/apt-get.sh
@@ -41,16 +41,11 @@ if [[ ! "$(type -P nvim)" ]]; then
 
     sudo apt-get -y install neovim
 
-    sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-    sudo update-alternatives --config vi
     sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
     sudo update-alternatives --config vim
     sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
     sudo update-alternatives --config editor
 
-    # vim plug
-    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 #python 3
