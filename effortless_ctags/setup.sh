421995b00c47bb2459e705e4b17ea8429fbb1fb4
Fmancino <17271080+Fmancino@users.noreply.github.com>
2018-08-21 21:50:14 +0200

    better setup for effortless_ctags

--- effortless_ctags/setup.sh
+++ effortless_ctags/setup.sh
@@ -7,23 +7,10 @@
 
 #CREATION DATE: 18-07-2018
 
-#LAST MODIFIED: Wed Jul 18 21:21:16 2018
+#LAST MODIFIED: Tue Aug 21 21:38:51 2018
 
 #AUTHOR: Francesco Mancino, francescoman@protonmail.com
 #-------------------------------------------------------------------------
-HOOKSDIR=~/.git_template/hooks
-EFFORTLESS_DOT=~/dotfiles/effortless_ctags/hooks
 
-git config --global init.templatedir '~/.git_template'
-mkdir -p $HOOKSDIR
+git config --global init.templatedir "$DOTFILES/effortless_ctags/git_template"
 git config --global alias.ctags '!.git/hooks/ctags'
-
-
-function interactive_copy()
-{ cp -i ${EFFORTLESS_DOT}/$1 ${HOOKSDIR}/$1; }
-
-interactive_copy ctags
-interactive_copy post-checkout
-interactive_copy post-commit
-interactive_copy post-merge
-interactive_copy post-rewrite
