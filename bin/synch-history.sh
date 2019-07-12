8b7ae176adc741251d9e073fae73f490df7af31e
Fmancino <17271080+Fmancino@users.noreply.github.com>
2019-07-12 11:14:56 +0200

    easy synch of of history

---
+++ bin/synch-history.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#CREATION DATE: 12-07-2019
+#PURPOSE: Synch this schell with the history file
+#-------------------------------------------------------------------------
+history -a && history -c && history -r
