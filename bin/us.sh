672c9ff3c69b4523aa4cc3d45682f702c20c498c
Fmancino <17271080+Fmancino@users.noreply.github.com>
2019-09-18 11:39:36 +0200

    international us

--- bin/us.sh
+++ bin/us.sh
@@ -3,5 +3,4 @@
 #CREATION DATE: 13-04-2019
 #PURPOSE:
 #-------------------------------------------------------------------------
-setxkbmap -layout us
-
+setxkbmap -layout us -variant altgr-intl -option nodeadkeys
