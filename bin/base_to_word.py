ccec082f94d189cff00bf9ac3f460eb8bd85c5d6
Fmancino <17271080+Fmancino@users.noreply.github.com>
2020-02-16 14:47:32 +0100

    make python script a valid module

--- bin/base_to_word.py
+++ bin/base_to_word.py
@@ -5,10 +5,10 @@ BASE_LENGTH = {2:8, # binary has usually 8 digits for a byte
                8:3, # base 8 has max 3 digits
                16:2} # base 16 has usually 2 digits
 
-def split_on_base(data, base):
+def split_on_base(number, base):
     data_list = []
     temp = ''
-    for c in data:
+    for c in number:
         temp += c
         if len(temp) == BASE_LENGTH[base]:
             data_list.append(temp)
@@ -17,6 +17,17 @@ def split_on_base(data, base):
         data_list.append(temp)
     return data_list
 
+def main(numbers, base):
+    ret = b''
+    if len(numbers) == 1:
+        in_data = split_on_base(numbers[0], base)
+    else:
+        in_data = numbers
+
+    for c in in_data:
+        ret += bytes(chr(int(c, base)), 'utf-8')
+    print(ret)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert a list of numbers into a word')
@@ -24,12 +35,4 @@ if __name__ == '__main__':
     parser.add_argument('-b', dest='base', type=int, default='16')
 
     args = parser.parse_args()
-    ret = b''
-    if len(args.numbers) == 1:
-        in_data = split_on_base(args.numbers[0], args.base)
-    else:
-        in_data = args.numbers
-
-    for c in in_data:
-        ret += bytes(chr(int(c,args.base)), 'utf-8')
-    print(ret)
+    main(args.numbers, args.base)
